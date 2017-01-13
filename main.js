3acc8a8cc4fb72604f2c1ebca8528f32e651115f
maiquel <maiquel_craash@hotmail.com>
2017-01-13 16:58:22 -0200

    Criado objeto Carousel para controlar o Dom respectivo

--- main.js
+++ main.js
@@ -5,21 +5,68 @@
 (function () {
 	"use strict";
 
-	var imgs = document.getElementById('slideshow').children;					// pega todos os imgs
-	var interval = 4000;														// duração do efeito
-	var currentPic = 0;
-	imgs[currentPic].style.webkitAnimation = 'fadey ' + interval + 'ms';		// anima a primeira imagem
-	imgs[currentPic].style.animation = 'fadey ' + interval + 'ms';
-	var infiniteLoop = setInterval(function () {								// loo infinito
-		imgs[currentPic].removeAttribute('style');								// remove o efeito da imagem atual
-		if (currentPic == imgs.length - 1) {									// se foi a última, volta pra primeira
-			currentPic = 0;
-		} else {
-			currentPic++;														// se não, avança a imagem
+
+	let carousel = {
+		imgs: document.getElementById('slideshow').children,											// pega todos os imgs
+		interval: 6000,
+		currentSlide: 0,
+		playEvent: "",
+		init: function () {
+			this.imgs[this.currentSlide].style.webkitAnimation = 'fadey ' + this.interval + 'ms';		// anima a primeira imagem
+			this.imgs[this.currentSlide].style.animation = 'fadey ' + this.interval + 'ms';
+			this.play();
+		},
+
+		nextSlide: function () {
+			this.imgs[this.currentSlide].removeAttribute('style');										// remove o efeito da imagem atual
+			if (this.currentSlide == this.imgs.length - 1) {											// se foi a última, volta pra primeira
+				this.currentSlide = 0;
+			}
+			else {
+				this.currentSlide++;																	// se não, avança a imagem
+			}
+			this.imgs[this.currentSlide].style.webkitAnimation = 'fadey ' + this.interval + 'ms';    	// adiciona efeito de animação na imagem atual
+			this.imgs[this.currentSlide].style.animation = 'fadey ' + this.interval + 'ms';
+		},
+
+		setSlide: function (index) {
+			this.stop();
+
+			this.imgs[this.currentSlide].removeAttribute('style');
+			if (index <= this.imgs.length - 1 && index >= 0) {
+				this.currentSlide = index;
+			}
+			else {
+				console.log("Invalid Carousel index");
+				this.currentSlide = 0;
+			}
+			this.imgs[this.currentSlide].style.webkitAnimation = 'fadey ' + this.interval + 'ms';    	// adiciona efeito de animação na imagem atual
+			this.imgs[this.currentSlide].style.animation = 'fadey ' + this.interval + 'ms';
+
+			this.play();
+		},
+
+		play: function () {
+			this.playEvent = setInterval(function () {
+				carousel.nextSlide();
+			}, carousel.interval);
+		},
+
+		stop: function () {
+			if (this.playEvent) {
+				clearInterval(this.playEvent);
+				this.playEvent = null;
+			}
+
 		}
-		imgs[currentPic].style.webkitAnimation = 'fadey ' + interval + 'ms';    // adiciona efeito de animação na imagem atual
-		imgs[currentPic].style.animation = 'fadey ' + interval + 'ms';
-	}, interval);
+	};
+
+	carousel.init();
+
+	//simula um clique depois de 5 s
+	setTimeout(function () {
+		carousel.setSlide(5);
+	}, 2000)
 
 
 }());
