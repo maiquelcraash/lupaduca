e30fb6cea6f45b899e8785aac66a62e43fcb1d9e
maiquel <maiquel_craash@hotmail.com>
2017-02-22 10:28:54 -0300

    Adicionadas Task para minificar HTML, otimizar imagens e uma tarefa que criar uma pasta para distribuição.

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,9 @@ const gulp = require('gulp'),
 	pkg = require('./package.json'),
 	clean = require('gulp-clean'),
 	babel = require('gulp-babel'),
-	zip = require('gulp-zip');
+	zip = require('gulp-zip'),
+	imagemin = require('gulp-imagemin'),
+	minifyHtml = require('gulp-html-minifier');
 
 // Set the banner content
 let banner = ['/*!\n',
@@ -38,7 +40,7 @@ gulp.task('clean', function () {
 });
 
 // Compile LESS files from /less into /css
-gulp.task('less', function () {
+gulp.task('less', () => {
 	return gulp.src(paths.lessDir + '/lupaduca.less')
 		.pipe(less())
 		.pipe(header(banner, {pkg: pkg}))
@@ -48,12 +50,19 @@ gulp.task('less', function () {
 		}))
 });
 
+// Minify HTML
+gulp.task('minify-html', () => {
+	return gulp.src('index.html')
+		.pipe(minifyHtml({collapseWhitespace: true, removeComments: true}))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest(''));
+});
+
 // Minify compiled CSS
-gulp.task('minify-css', ['less', 'clean'], function () {
+gulp.task('minify-css', ['less'], () => {
 	return gulp.src(paths.cssDir + '/lupaduca.css')
 		.pipe(cleanCSS({compatibility: 'ie8'}))
 		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest(paths.distDir + '/css'))
 		.pipe(gulp.dest(paths.cssDir))
 		.pipe(browserSync.reload({
 			stream: true
@@ -68,12 +77,11 @@ gulp.task('build', () => {    //add "lint" if you want to use de eslinter
 });
 
 // Minify JS
-gulp.task('minify-js', ['clean', 'build'], () => {
+gulp.task('minify-js', ['build'], () => {
 	return gulp.src(paths.libDir + '/core/animate.js')
 		.pipe(uglify())
 		.pipe(header(banner, {pkg: pkg}))
 		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest(paths.distDir + '/js'))
 		.pipe(gulp.dest(paths.srcDir))
 		.pipe(browserSync.reload({
 			stream: true
@@ -81,18 +89,14 @@ gulp.task('minify-js', ['clean', 'build'], () => {
 });
 
 // Copy vendor libraries from /node_modules into /vendor
-gulp.task('copy' , ['clean', 'minify-css', 'minify-js'], function () {
+gulp.task('copy', ['clean', 'minify-css', 'minify-js'], () => {
+
+	//Copy Libs
 	gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
 		.pipe(gulp.dest('lib/bootstrap'));
 
 	gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
 		.pipe(gulp.dest('lib/jquery'));
-
-	gulp.src(['node_modules/tether/dist/js/tether.js', 'node_modules/tether/dist/js/tether.min.js'])
-		.pipe(gulp.dest('lib/tether'));
-
-	gulp.src(['index.html'])
-		.pipe(gulp.dest(paths.distDir));
 
 	gulp.src([paths.libDir + '/bootstrap/js/bootstrap.min.js'])
 		.pipe(gulp.dest(paths.distDir + '/lib/bootstrap/js'));
@@ -103,9 +107,24 @@ gulp.task('copy' , ['clean', 'minify-css', 'minify-js'], function () {
 	gulp.src([paths.libDir + '/jquery/jquery.min.js'])
 		.pipe(gulp.dest(paths.distDir + '/lib/jquery'));
 
-	gulp.src([paths.imgDir + '/**'])
-		.pipe(gulp.dest(paths.distDir + '/img'));
+	//Copy Htmls
+	gulp.src(['index.min.html'])
+		.pipe(rename('index.html'))
+		.pipe(gulp.dest(paths.distDir));
 
+	//Copy CSS
+	gulp.src(paths.cssDir + "/lupaduca.min.css")
+		.pipe(gulp.dest(paths.distDir + '/css'));
+
+	//Copy JS
+	gulp.src(paths.srcDir + "/animate.min.js")
+		.pipe(gulp.dest(paths.distDir + '/js'));
+
+	//Copy Images
+	// gulp.src([paths.imgDir + '/**'])
+	// 	.pipe(gulp.dest(paths.distDir + '/img'));
+
+	//Copy Fonts
 	gulp.src([
 		'node_modules/font-awesome/**',
 		'!node_modules/font-awesome/**/*.map',
@@ -117,8 +136,19 @@ gulp.task('copy' , ['clean', 'minify-css', 'minify-js'], function () {
 		.pipe(gulp.dest('lib/font-awesome'))
 });
 
+
+// Optimize images
+gulp.task('image-min', ['clean'], () => {
+	return gulp.src(paths.imgDir + '/**')
+		.pipe(imagemin())
+		.pipe(gulp.dest(paths.distDir + '/img'));
+});
+
 // Run everything
-gulp.task('default', ['clean', 'less', 'minify-css', 'build', 'minify-js', 'copy']);
+gulp.task('default', ['less', 'minify-css', 'build', 'minify-js', 'minify-html']);
+
+// Run production task
+gulp.task('dist', ['clean', 'default', 'copy', 'image-min']);
 
 // Configure the browserSync task
 gulp.task('browserSync', function () {
